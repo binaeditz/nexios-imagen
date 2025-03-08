dcd528cf174fff4accb640ac9830cd4a552c53fa
Binara Denuwan <166313305+binaeditz@users.noreply.github.com>
2025-03-08 21:22:28 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "AI SDK Image Generator",
-  description: "An open-source AI image generator using the AI SDK by Vercel",
+  title: "Nexios-Imagen",
+  description: "An open-source AI image generator by Nexios",
 };
 
 export default function RootLayout({
